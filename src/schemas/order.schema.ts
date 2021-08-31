6cb27eb06449694d7ab659b90b8eb5243bf4a107
JulianMiranda <julian@wayu2.com>
2021-08-30 21:15:52 -0500

    Order owner

--- src/schemas/order.schema.ts
+++ src/schemas/order.schema.ts
@@ -3,6 +3,7 @@ import { schemaOptions } from '../utils/index';
 
 export const OrderSchema = new mongoose.Schema(
   {
+    owner: { type: String, default: 'Julian' },
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', index: true },
     car: [],
     cost: Number,
